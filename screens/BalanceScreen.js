30e3f90a8bd76138872606ba501be74f51ab0ade
paiizz <wanchanapon.t@ku.th>
2018-04-19 13:39:55 +0700

    get data from firebase

--- screens/BalanceScreen.js
+++ screens/BalanceScreen.js
@@ -1,11 +1,48 @@
-import React, { Component } from 'react';
-import { Text} from 'react-native';
+import React, { Component } from "react";
+import {Text, View, FlatList} from "react-native";
+import {List, ListItem} from "react-native-elements";
+import firebase from 'firebase';
 
 class BalanceScreen extends Component {
-    render() {
-        return (
-          <Text>Another Screen</Text>  
-        );
+  static navigationOptions = {
+    title: 'New Feeds',
+    header: null
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+        data: []
     }
+  }
+
+  componentDidMount(){
+    const database = firebase.database().ref('/users')
+    database.on('value', snap =>{
+      snap.forEach(element => {
+         this.setState({ data : [ ...this.state.data, element.val().userDetails] })
+      });
+    })
+  }
+
+  render() {
+    return (
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0}}>
+      <FlatList
+        data = {this.state.data}
+        renderItem={
+          ({item}) => (
+          <ListItem
+          roundAvatar
+          title={`${item.displayName}`}
+          subtitle={item.email}
+          containerStyle={{borderBottomWidth:0}}
+          />
+        )}
+        keyExtractor={item => item.email}
+      />
+      </List>
+    )  
+  }
 }
 export default BalanceScreen;
